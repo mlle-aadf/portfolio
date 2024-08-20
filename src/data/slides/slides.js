af6b93649cb64ab95c2b251674efa795ad4e473c
Alexandra <105469117+mlle-aadf@users.noreply.github.com>
2024-08-20 18:32:45 -0400

    replaced blackJack with Fluffy

--- src/data/slides/slides.js
+++ src/data/slides/slides.js
@@ -2,15 +2,15 @@ import bunny_reloaded from "../slides/bunny_reloaded.gif"
 import password_generator from "../slides/password_generator.png"
 import dice from "../slides/dice.gif"
 import jelly_form from "../slides/jelly_form.png"
-import black_jack from "../slides/black_jack.png"
+import fluffy_donuts from "../slides/fluffys_home.png"
 import jellery from "../slides/jellery.png"
 
 const slides = [
         
+    {src:`${fluffy_donuts}`, title: "fluffy_donuts", alt: "fluffy's landing page", link: "https://github.com/Jbacani4/FluffyDonuts"}, 
     {src: `${jellery}`, title: "jelly_gallery", alt: "photo gallery", link: "https://codepen.io/mlle-aadf/full/PoXmrNK"},
     {src: `${bunny_reloaded}`, title: "bunny_reloaded", alt: "bunny game", link:"https://bunny-reloaded.vercel.app/"}, 
     {src: `${password_generator}`, title: "password_generator", alt: "password generator", link:"https://codepen.io/mlle-aadf/full/qBLKWzO"}, 
-    {src:`${black_jack}`, title: "black_jack", alt: "blackjack game", link: "https://codepen.io/mlle-aadf/full/eYbWwdo"}, 
     {src:`${dice}`, title: "dice", alt:"dice", link: "https://codepen.io/mlle-aadf/full/VwqwNOZ"},
     {src: `${jelly_form}`, title: "jelly_form", alt: "html form", link: "https://codepen.io/mlle-aadf/full/MWzMMOv"},
 ]
