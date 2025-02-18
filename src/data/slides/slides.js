66ced598ee87983586c65c5f5ce6427987790659
Alexandra <105469117+mlle-aadf@users.noreply.github.com>
2025-02-17 21:28:38 -0500

    update fluffy url

--- src/data/slides/slides.js
+++ src/data/slides/slides.js
@@ -1,13 +1,13 @@
 import bunny_reloaded from "../slides/bunny_reloaded.gif"
-import password_generator from "../slides/password_generator.png"
 import dice from "../slides/dice.gif"
-import jelly_form from "../slides/jelly_form.png"
 import fluffy_donuts from "../slides/fluffys_home.png"
 import jellery from "../slides/jellery.png"
+import jelly_form from "../slides/jelly_form.png"
+import password_generator from "../slides/password_generator.png"
 
 const slides = [
         
-    {src:`${fluffy_donuts}`, title: "fluffy_donuts", alt: "fluffy's landing page", link: "https://github.com/Jbacani4/FluffyDonuts"}, 
+    {src:`${fluffy_donuts}`, title: "fluffy_donuts", alt: "fluffy's landing page", link: "https://www.fluffys.ca/"}, 
     {src: `${jellery}`, title: "jelly_gallery", alt: "photo gallery", link: "https://codepen.io/mlle-aadf/full/PoXmrNK"},
     {src: `${bunny_reloaded}`, title: "bunny_reloaded", alt: "bunny game", link:"https://bunny-reloaded.vercel.app/"}, 
     {src: `${password_generator}`, title: "password_generator", alt: "password generator", link:"https://codepen.io/mlle-aadf/full/qBLKWzO"}, 
