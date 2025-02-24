3fed0a7ca363191df13cdff63a5201b848b36969
Alexandra <105469117+mlle-aadf@users.noreply.github.com>
2025-02-23 23:52:21 -0500

    add echoes slide

--- src/data/slides/slides.js
+++ src/data/slides/slides.js
@@ -1,6 +1,7 @@
 import adhd from "../slides/adhd_bujo.png"
 import bunny_reloaded from "../slides/bunny_reloaded.gif"
 import dice from "../slides/dice.gif"
+import echoes from "../slides/echoes.png"
 import ewear from "../slides/ewear_emporium.png"
 import fluffy_donuts from "../slides/fluffys_home.png"
 import jellery from "../slides/jellery.png"
@@ -11,6 +12,7 @@ const slides = [
         
     {src:`${fluffy_donuts}`, title: "fluffy_donuts", alt: "fluffy's landing page", link: "https://www.fluffys.ca/"}, 
     {src:`${ewear}`, title: "ewear_emporium", alt: "e-commerce", link: "https://e-wearclient.vercel.app/"}, 
+    {src:`${echoes}`, title: "echoes", alt: "multi-ai query app", link: "https://echoes-ai.vercel.app/"}, 
     {src:`${adhd}`, title: "adhd_bujo", alt: "CRUD app", link: "https://github.com/mlle-aadf/adhd_bujo"}, 
     {src: `${jellery}`, title: "jelly_gallery", alt: "photo gallery", link: "https://codepen.io/mlle-aadf/full/PoXmrNK"},
     {src: `${bunny_reloaded}`, title: "bunny_reloaded", alt: "bunny game", link:"https://bunny-reloaded.vercel.app/"}, 
